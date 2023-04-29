dcfae51c75737fa75452fd946593d34c37d609d5
DylanIsAKingL <114356367+DylanIsAKingL@users.noreply.github.com>
2023-04-29 11:03:30 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,6 @@
 local repo = "https://raw.githubusercontent.com/DylanIsAKingL/KingsWare/main/"
 if game.PlaceId == 7591780014 then
     loadstring(game:HttpGet(repo.."FortBlox.lua"))()
+elseif game.PlaceId == 621129760 then
+    loadstring(game:HttpGet(repo.."KAT.lua"))()
 end
